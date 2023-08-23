0b435bc2bec469111bdda5f6f3ecb322f0c45c61
aditya9s <132540655+aditya9s@users.noreply.github.com>
2023-08-24 02:57:02 +0530

    Update api.py

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-origins = ['http://localhost:5173', 'http://localhost:3001', 'http://localhost:3003']
+origins = ['http://localhost:5173', 'http://localhost:3001', 'https://grow-me-organics.netlify.app/']
 CORS(app, origins=origins)
 
 # Sample data
